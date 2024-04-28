337fe2d98cafeb14ce9ae98e4b3c9632e57c1d5b
Johan Bouduin <johan@der-e-coach.de>
2024-04-28 13:51:04 +0200

    Install forge-publisher and update-electron-app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Collection Manager
+
+Currently just playing around with Electron and publishing resp. updating electron app.
--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron/main')
+const path = require('node:path')
+
+require('update-electron-app')()
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: "/resources/icons/collection_manager_512",
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
 }
 
+ipcMain.handle('dark-mode:toggle', () => {
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = 'light'
+  } else {
+    nativeTheme.themeSource = 'dark'
+  }
+  return nativeTheme.shouldUseDarkColors
+})
+
+ipcMain.handle('dark-mode:system', () => {
+  nativeTheme.themeSource = 'system'
+})
+
+
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
 
   app.on('activate', () => {

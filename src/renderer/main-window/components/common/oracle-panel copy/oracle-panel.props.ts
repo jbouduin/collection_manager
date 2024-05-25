61f6a26d39a8821ae92d618638156f404b9bab92
Johan Bouduin <johan@der-e-coach.de>
2024-05-25 12:52:44 +0200

    removed redundant file

--- src/renderer/main-window/components/common/oracle-panel copy/oracle-panel.props.ts
+++
@@ -1,7 +0,0 @@
-import { Props } from "@blueprintjs/core";
-
-import { CardViewmodel } from "../../../viewmodels/card.viewmodel";
-
-export interface OraclePanelProps extends Props {
-  card?: CardViewmodel;
-}

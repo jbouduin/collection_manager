c8cb63b25ea9a4bc6f88b724dc7f5ebf9b1f3281
Johan Bouduin <johan@der-e-coach.de>
2024-05-25 14:25:21 +0200

    Display symbols in card text

--- src/renderer/main-window/components/common/card-detail-panel/card-text/card-text.tsx
+++ src/renderer/main-window/components/common/card-detail-panel/card-text/card-text.tsx
@@ -1,14 +1,36 @@
 import * as React from "react";
 
 import { CardTextProps } from "./card-text.props";
+import { SvgProvider } from "../../svg-provider/svg-provider";
 
 
 export function CardText(props: CardTextProps) {
   //#region Main --------------------------------------------------------------
   return (
     <div>
-      {props.cardText.replace(/\r\n/g, "\r").replace(/\n/g, "\r").split(/\r/).map((t: string) => <p>{t}</p>)}
+      {
+        props.cardText
+          .replace(/\r\n/g, "\r")
+          .replace(/\n/g, "\r")
+          .split(/\r/)
+          .map((paragraph: string) => <p>{splitParagraph(paragraph)}</p>)
+      }
     </div>
   );
+  //#endregion
+
+  //#region Helper functions --------------------------------------------------
+  function splitParagraph(paragraph: string): Array<React.JSX.Element | string> {
+    const matches = paragraph.match(/{[^}]*}|[^{}]+/gmi);
+
+    return matches.map((match: string) => {
+      if (match.startsWith("{") && match.endsWith("}")) {
+        const svg = props.symbolSvgs.get(match);
+        return (<SvgProvider className={props.className} svg={svg} />);
+      } else {
+        return match;
+      }
+    });
+  }
   //#endregion
 }

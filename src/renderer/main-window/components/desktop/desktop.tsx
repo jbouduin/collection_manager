692664d1fba84acf1b51109e539aa4f442accdeb
Johan Bouduin <johan@der-e-coach.de>
2024-06-04 10:47:06 +0200

    Update desktop.tsx

--- src/renderer/main-window/components/desktop/desktop.tsx
+++ src/renderer/main-window/components/desktop/desktop.tsx
@@ -1,14 +1,14 @@
-import { Card, Classes } from "@blueprintjs/core";
+import { Card } from "@blueprintjs/core";
 import * as React from "react";
 
+import classNames from "classnames";
+import { DARK_THEME, ThemeContext } from "../theme";
 import { CollectionView } from "../views/collection-view/collection-view";
 import { DatabaseView } from "../views/database-view/database-view";
 import { DeckView } from "../views/deck-view/deck-view";
 import { ButtonBar } from "./button-bar";
 import { EDesktopView } from "./desktop-view.enum";
 import { DesktopProps } from "./desktop.props";
-import classNames from "classnames";
-import { DARK_THEME, ThemeContext } from "../theme";
 
 // import logo from "./logo.png";
 
@@ -51,22 +51,3 @@ export function Desktop(props: DesktopProps) {
   );
   //#endregion
 }
-
-/* <header> *
-        <Navbar className={this.theme} fixedToTop={true}>
-          <NavbarGroup align={Alignment.LEFT} className={this.theme}>
-            <Icon icon="globe" />
-            <NavbarDivider />
-            <NavbarHeading>Magic assistant</NavbarHeading>
-            <NavbarDivider />
-            <AnchorButton text="MTG Database" minimal />
-            <AnchorButton text="Collections" minimal />
-            <AnchorButton text="Decks" minimal />
-          </NavbarGroup>
-          <Navbar.Group align={Alignment.RIGHT} className={this.theme}>
-            <Button className="bp3-minimal" icon="notifications" />
-            <Button className="bp3-minimal" icon={this.themeIcon} onClick={() => window.ipc.darkmode("toggle").then((value: boolean) => this.setTheme(value))} />
-            <Button className="bp3-minimal" icon="cog" onClick={() => window.ipc.darkmode("toggle").then((value: boolean) => this.setTheme(value))} />
-          </Navbar.Group>
-        </Navbar>
-         </header> */

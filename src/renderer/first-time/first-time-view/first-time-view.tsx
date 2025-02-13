2d17050fd212b1f2e78412ee0f39492299cc40ca
Johan Bouduin <johan@der-e-coach.de>
2025-02-13 18:34:06 +0100

    Provide DisplayValueServiceContext in first time window

--- src/renderer/first-time/first-time-view/first-time-view.tsx
+++ src/renderer/first-time/first-time-view/first-time-view.tsx
@@ -3,19 +3,22 @@ import classNames from "classnames";
 import * as React from "react";
 import { ConfigurationWrapper } from "../../common/components/configuration/configuration-wrapper/configuration-wrapper";
 import { FirstTimeViewProps } from "./first-time-view.props";
+import { DisplayValueService, DisplayValueServiceContext } from "../../common/context";
 
 export function FirstTimeView(props: FirstTimeViewProps) {
-  //#region Main --------------------------------------------------------------
+  //#region Rendering ---------------------------------------------------------
   return (
-    <Card className={classNames(props.className, "desktop-wrapper")} compact={true}>
-      <Section compact={true}>
-        <ConfigurationWrapper
-          configuration={props.configuration}
-          onCancel={() => window.close()}
-          onSave={() => window.close()}
-        />
-      </Section>
-    </Card>
+    <DisplayValueServiceContext.Provider value={new DisplayValueService()}>
+      <Card className={classNames(props.className, "desktop-wrapper")} compact={true}>
+        <Section compact={true}>
+          <ConfigurationWrapper
+            configuration={props.configuration}
+            onCancel={() => window.close()}
+            onSave={() => window.close()}
+          />
+        </Section>
+      </Card>
+    </DisplayValueServiceContext.Provider>
   );
   //#endregion
 }
